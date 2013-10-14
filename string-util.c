aaf45571bc98b615cc59118ef31162ed38c1cd15
JackABK <abkabkabkyes@gmail.com>
2013-10-15 04:09:43 +0800

    add the string function to string-util.c (make up the mistake of last commit)

--- string-util.c
+++ string-util.c
@@ -67,3 +67,17 @@ char *strncpy(char *dest, const char *src, size_t n)
 	while (n-- && (*d++ = *s++));
 	return dest;
 }
+int strlen(char *str )
+{
+  int i = 0 ;
+  while(str[i]!='\0'){
+    i++;
+  }
+  return i ;
+}
+int strcmp(const char* s1, const char* s2)
+{
+    while(*s1 && (*s1==*s2))
+        s1++,s2++;
+    return *(const unsigned char*)s1-*(const unsigned char*)s2;
+}
